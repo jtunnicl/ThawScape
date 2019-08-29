de6f9bc4dbb32ccf64a5f2ee9fa32d72e14bf5b5
Chris Scott <chris@chrisdjscott.co.uk>
2019-08-30 11:13:23 +1200

    Updating plot script to accept multiple files

--- tools/plot_raster.py
+++ tools/plot_raster.py
@@ -7,48 +7,67 @@ Contour plot of a raster.
 """
 import os
 import argparse
-import glob
 
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
 
-# parse args
-parser = argparse.ArgumentParser(description="Contour plot of a raster")
-parser.add_argument('filename', help="File containing Raster to plot")
-parser.add_argument('-l', dest='logscale', action='store_true', help='Plot on logarithmic scale')
-parser.add_argument('-s', dest='save', action='store_true', help="Save the image to file")
-parser.add_argument('-o', dest='offline', action='store_true', help="Don't display interactive plot")
-args = parser.parse_args()
-fn = args.filename
-assert os.path.exists(fn), "File does not exist: {}".format(fn)
+def plot_file(fn, args):
+    print("Plotting file: {}".format(fn))
 
-# load data, first read nodata value
-with open(fn) as fh:
-    for i in range(5):
-        fh.readline()
-    nodata = float(fh.readline().split()[-1])
+    # load data, first read nodata value
+    with open(fn) as fh:
+        for i in range(5):
+            fh.readline()
+        nodata = float(fh.readline().split()[-1])
 
-# now read data
-data = np.loadtxt(fn, skiprows=6)
+    # now read data
+    data = np.loadtxt(fn, skiprows=6)
 
-# fix nodata
-if np.any(data == nodata):
-    data[data == nodata] = np.nan
+    # fix nodata
+    if np.any(data == nodata):
+        data[data == nodata] = np.nan
 
-# now plot
-fig = plt.figure(figsize=(12, 8))
-ax = fig.add_subplot(111)
-if args.logscale:
-    plt.contourf(data, cmap='seismic', locator=matplotlib.ticker.LogLocator())
-else:
-    plt.contourf(data, cmap='seismic')
-cbar = plt.colorbar()
-plt.gca().set_aspect('equal', adjustable='box')
-plt.title(fn)
-plt.tight_layout()
-if args.save:
-    plt.savefig(fn + ".png")
-if not args.offline:
-    plt.show()
+    # make the plot
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(111)
+    if args.logscale:
+        plt.contourf(data, cmap='seismic', locator=matplotlib.ticker.LogLocator())
+    else:
+        plt.contourf(data, cmap='seismic')
+    cbar = plt.colorbar()
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.title(fn)
+    plt.tight_layout()
+
+    # save to file?
+    if args.save:
+        savefn = fn + ".png"
+        print("Saving plot to: {}".format(savefn))
+        plt.savefig(savefn)
+
+    # view interactive?
+    if not args.offline:
+        plt.show()
+
+    # clear figure
+    plt.clf()
+
+
+def main():
+    # parse args
+    parser = argparse.ArgumentParser(description="Create contour plots of raster files (multiple files can be specified)")
+    parser.add_argument('filename', nargs='+', help="File containing Raster to plot")
+    parser.add_argument('-l', '--logscale', action='store_true', help='Plot on logarithmic scale (default is linear)')
+    parser.add_argument('-s', '--save', action='store_true', help="Save the image to file (default is not to save to file)")
+    parser.add_argument('-o', '--offline', action='store_true', help="Don't display interactive plot (default is to display the interactive plot)")
+    args = parser.parse_args()
+
+    # plot all files specified
+    for fn in args.filename:
+        plot_file(fn, args)
+
+
+if __name__ == "__main__":
+    main()
