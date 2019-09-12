c239a71ad22947378c2eedcbc15a0b4d08c847de
Chris Scott <chris@chrisdjscott.co.uk>
2019-09-12 15:34:05 +1200

    Option to plot differences from a given Raster

--- tools/plot_raster.py
+++ tools/plot_raster.py
@@ -13,10 +13,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def plot_file(fn, args):
-    print("Plotting file: {}".format(fn))
-
-    # load data, first read nodata value
+def load_raster(fn):
     with open(fn) as fh:
         for i in range(5):
             fh.readline()
@@ -28,6 +25,23 @@ def plot_file(fn, args):
     # fix nodata
     if np.any(data == nodata):
         data[data == nodata] = np.nan
+
+    return data
+
+
+def plot_file(fn, args, data_diff):
+    print("Plotting file: {}".format(fn))
+
+    # load data, first read nodata value
+    data = load_raster(fn)
+
+    # are we plotting differences
+    if data_diff is not None:
+        data -= data_diff
+
+    # plot absolute values
+    if args.abs:
+        data = np.absolute(data)
 
     # make the plot
     fig = plt.figure(figsize=(12, 8))
@@ -64,11 +78,24 @@ def main():
     parser.add_argument('-l', '--logscale', action='store_true', help='Plot on logarithmic scale (default is linear)')
     parser.add_argument('-s', '--save', action='store_true', help="Save the image to file (default is not to save to file)")
     parser.add_argument('-o', '--offline', action='store_true', help="Don't display interactive plot (default is to display the interactive plot)")
+    parser.add_argument('-d', '--diff', default=None, help="Plot the difference from the file specified by this option (default is not to plot differences)")
+    parser.add_argument('-a', '--abs', action='store_true', help="Plot absolute values (default is not to take absolute values)")
     args = parser.parse_args()
+
+    # are we plotting differences
+    if args.diff is not None:
+        print("Plotting differences of Rasters relative to '{}''".format(args.diff))
+        data_diff = load_raster(args.diff)
+    else:
+        data_diff = None
+
+    # absolute values
+    if args.abs:
+        print("Plotting absolute values")
 
     # plot all files specified
     for fn in args.filename:
-        plot_file(fn, args)
+        plot_file(fn, args, data_diff)
 
 
 if __name__ == "__main__":
